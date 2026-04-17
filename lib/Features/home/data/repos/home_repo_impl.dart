3a388707552dcc5c2b94db3b08f3dc640176d676
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2026-04-17 14:39:28 +0200

    feat: enhance error handling in HomeRepoImpl with specific DioException cases

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -21,7 +22,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(Failure(message: ''));
     }
   }
 
@@ -36,7 +40,7 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      return left(Failure(message: ''));
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,54 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+class Failure {
+  final String message;
+
+  Failure({required this.message});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.message});
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(message: 'Connection timeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(message: 'Send timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(message: 'Receive timeout');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromStatusCode(dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(message: 'Request cancelled');
+      case DioExceptionType.unknown:
+      default:
+        return ServerFailure(message: 'Unexpected error occurred');
+    }
+  }
+
+  factory ServerFailure.fromStatusCode(int statusCode, dynamic data) {
+    switch (statusCode) {
+      case 400:
+        return ServerFailure(message: 'Bad request, ${data['error']['message']}');
+      case 401:
+        return ServerFailure(message: 'Unauthorized, ${data['error']['message']}');
+      case 403:
+        return ServerFailure(message: 'Forbidden, ${data['error']['message']}');
+      case 404:
+        return ServerFailure(message: 'Your request was Not found');
+      case 500:
+        return ServerFailure(message: 'Internal server error');
+      default:
+        return ServerFailure(message: 'There is an error with status code: $statusCode');
+    }
+  }
+}
+
+class CacheFailure extends Failure {
+  CacheFailure({required super.message});
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure({required super.message});
+}
