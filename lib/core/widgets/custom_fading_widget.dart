eb10e8ad17474b153d962c51ad3a5d978496b77f
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2026-04-24 19:09:52 +0300

    feat: add CustomFadingWidget for animated opacity transitions

---
+++ lib/core/widgets/custom_fading_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomFadingWidget extends StatefulWidget {
+  const CustomFadingWidget({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<CustomFadingWidget> createState() => _CustomFadingWidgetState();
+}
+
+class _CustomFadingWidgetState extends State<CustomFadingWidget> with SingleTickerProviderStateMixin {
+  late Animation fadeAnimation;
+  late AnimationController animationController;
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this);
+    fadeAnimation = Tween<double>(begin: 0.2, end: 0.8).animate(animationController);
+    animationController.repeat(reverse: true);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: fadeAnimation,
+      builder: (context, child) => Opacity(
+        opacity: fadeAnimation.value,
+        child: widget.child,
+      ),
+    );
+  }
+}
